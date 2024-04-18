8eda1d7bba781f7d2ae9c885096ec008827636db
Luis Fernando <luisfer.th93@gmail.com>
2024-04-17 18:00:03 -0600

    mysqli

--- html/conn.php
+++ html/conn.php
@@ -1,15 +1,19 @@
 <?php
-$servername = "localhost:3035";
-$username = "yun";
-$password = "tianming";
-$dbname = "rpg";
 $conn_local = null;
 
-function get_conn(){
-    if ($conn_local == null){
-        phpinfo();
-        $conn_local = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+function get_conn() {
+    $servername = "db";
+    $username = "yun";
+    $password = "tianming";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=rpg", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+        return $conn;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
     }
-    return $conn_local;
 }
 ?>
